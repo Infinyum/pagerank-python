5f506a983e2c58340f3fa1c39172d11958071da1
T4ouf <thomasvonascheberg@hotmail.fr>
2019-10-18 21:16:52 +0200

    add first draft of convergence test

--- src/benchmarks.py
+++ src/benchmarks.py
@@ -1,8 +1,68 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from main import *
+from matrix import *
 import time
 
-def sparseMatrixBenchmark(matrix,damping=0.85):
+#Convention : 	function_sm => for sparse matrices
+#				function_m => for the other matrices
+
+def iterativeConvergenceConstDamping(matrix,damping=0.85,itLimit=100,):
+
+	N = matrix.shape[0]
+	exactResult = pageRankMarkov(matrix, damping)
+
+	#populationVector = generateProbabilisticVector(N, False)
+
+	#We generate a simple population vector (1,0,0,0,0,...,0)
+	populationVector = np.zeros(N)
+	populationVector[0] = 1.0
+
+	#We are going to compute the evolution of the difference between the exact solution and the Kth iteration
+	differencesVector = np.zeros(100) 
+
+	k = itLimit
+
+	#Compute the kth iterations 
+	while(k>0):
+
+		populationAfterKIteration = pageRankMarkovByStep(matrix, populationVector, damping, k)
+		differencesVector[k-1] = np.average(np.abs(exactResult.T[0] - populationAfterKIteration))
+		k = k - 1
+
+	#PLOT PART
+
+	maxVal = np.amax(differencesVector)
+
+	x = [0,itLimit]
+	y = [exactResult,exactResult]
+
+	iterations = np.linspace(0,itLimit,100)
+
+	axes = plt.gca()
+	axes.set_xlim([0,itLimit+2])
+	#axes.set_ylim([0, maxVal])
+
+	plt.figure(1)
+	plt.plot(iterations,differencesVector,'b-',label='Distance to the exact solution')
+	plt.legend()
+	plt.title("Average Distance to the exact solution depending on the number of iteration")       #Adding a title
+	plt.xlabel("Number of iteration") 	#X axis title
+	plt.ylabel("Distance") 				#Y axis title
+
+	plt.yscale('log')
+
+	plt.show()
 
 
-    #We are going to benchmark
-    start_time = time.time()
+
+
+
+#matrix = createMatrix()
+matrix = generateRandomStandardizedLinkMatrix(100,False,True)
+
+iterativeConvergenceConstDamping(matrix,0.85,100)
+
+#Input in order to keep the plot alive
+t = input("Press Enter to finish the programm...")
