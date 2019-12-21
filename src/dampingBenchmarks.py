7c00fa03b10b5b52bbe0fd5b6fa36af1cbc630ac
T4ouf <thomasvonascheberg@hotmail.fr>
2019-12-21 15:10:12 +0100

    fix some plot issues

--- src/dampingBenchmarks.py
+++ src/dampingBenchmarks.py
@@ -9,19 +9,15 @@ def rankEvolutionDamping(matrix,itLimit=100):
 
 	N = matrix.shape[0]
 
-	#We generate a simple population vector (1,0,0,0,0,...,0)
-	populationVector = np.zeros(N)
-	populationVector[0] = 1.0
-
 	pointsNum = 50
 
 	#We are going to compute the evolution of the difference between the exact solution and the Kth iteration
 	rankPageOne = np.zeros(pointsNum) 
 
-	initDamping = 0.51
+	initDamping = 0.0
 	index = 0
 
-	step = (1-initDamping) / pointsNum
+	step = (1.0-initDamping) / pointsNum
 
 	damping = initDamping
 	while damping < 1.0:
@@ -38,7 +34,7 @@ def rankEvolutionDamping(matrix,itLimit=100):
 
 	axes = plt.gca()
 	axes.set_xlim([0,1])
-	axes.set_ylim([0, 2*1e-16])
+	#axes.set_ylim([0, 2*1e-16])
 
 	plt.figure(1)
 	plt.plot(dampingAxis,rankPageOne,'r-',label='Rank of the first page')
