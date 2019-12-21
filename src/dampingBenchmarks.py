3f3338da198fb2ef597384e4b42f6d8cd879fa07
Robin <malmasson.robin@gmail.com>
2019-12-21 14:57:50 +0100

    Added damping benchmark

---
+++ src/dampingBenchmarks.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from main import *
+from matrix import *
+import time
+
+def rankEvolutionDamping(matrix,itLimit=100):
+
+	N = matrix.shape[0]
+
+	#We generate a simple population vector (1,0,0,0,0,...,0)
+	populationVector = np.zeros(N)
+	populationVector[0] = 1.0
+
+	pointsNum = 50
+
+	#We are going to compute the evolution of the difference between the exact solution and the Kth iteration
+	rankPageOne = np.zeros(pointsNum) 
+
+	initDamping = 0.51
+	index = 0
+
+	step = (1-initDamping) / pointsNum
+
+	damping = initDamping
+	while damping < 1.0:
+
+		exactResult  = pageRankMarkov(matrix, damping)
+		rankPageOne[index] = exactResult[0]
+		#print(rankPageOne[index])
+
+		damping = damping + step
+		index = index + 1
+
+
+	dampingAxis = np.linspace(0,1,pointsNum)
+
+	axes = plt.gca()
+	axes.set_xlim([0,1])
+	axes.set_ylim([0, 2*1e-16])
+
+	plt.figure(1)
+	plt.plot(dampingAxis,rankPageOne,'r-',label='Rank of the first page')
+	plt.legend()
+	plt.title("Evolution of the rank of a page according to the damping")       #Adding a title
+	plt.xlabel("Damping") 	#X axis title
+	plt.ylabel("Rank") 	#Y axis title
+	plt.show()
+
+
+matrix = generateRandomStandardizedLinkMatrix(100,True,True)
+
+rankEvolutionDamping(matrix,100)
